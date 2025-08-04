a0eb0acff8d9ccadfff092c6bf833f06a4040d11
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-08-04 14:11:30 +0200

    Update DoguPipe.groovy

--- src/com/cloudogu/sos/pipebuildlib/DoguPipe.groovy
+++ src/com/cloudogu/sos/pipebuildlib/DoguPipe.groovy
@@ -176,10 +176,12 @@ end
         return new Docker(script).image("ghcr.io/tcort/markdown-link-check:${markdownVersion}")
             .mountJenkinsUser()
             .inside("--entrypoint=\"\" -v ${script.env.WORKSPACE}/docs:/docs") {
-                return script.sh '''
-                    find /docs -name \\*.md -print0 | xargs -0 -n1 markdown-link-check -v
-                ''',
-                returnStatus: true
+                return script.sh(
+                    script: '''
+                        find /docs -name \\*.md -print0 | xargs -0 -n1 markdown-link-check -v
+                    ''',
+                    returnStatus: true
+                )
             }
     }
 
