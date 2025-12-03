c2b398d9d6e3ade9edb83432014eec14e287b73e
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-12-03 14:06:39 +0100

    Change Google disk type from 'pd-ssd' to 'pd-extreme'

--- src/com/cloudogu/sos/pipebuildlib/DoguPipe.groovy
+++ src/com/cloudogu/sos/pipebuildlib/DoguPipe.groovy
@@ -159,7 +159,7 @@ Vagrant.configure(2) do |config|
     google.image_family = 'sos-development2404'
     google.zone = "europe-west3-a"
     google.machine_type = "${machineType}"
-    google.disk_type = "pd-ssd"
+    google.disk_type = "pd-extreme"
     # preemptible
     google.preemptible = true
     google.auto_restart = false
