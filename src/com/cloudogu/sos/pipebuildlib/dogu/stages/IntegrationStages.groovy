3d67e24a7be4e3b05d49b78da9ff88e5ac8b4791
Dennis Schwarzer <dennis.schwarzer@cloudogu.com>
2026-01-12 14:56:21 +0100

    big refactor

--- src/com/cloudogu/sos/pipebuildlib/dogu/stages/IntegrationStages.groovy
+++ src/com/cloudogu/sos/pipebuildlib/dogu/stages/IntegrationStages.groovy
@@ -1,7 +1,7 @@
 package com.cloudogu.sos.pipebuildlib.stages
 
 import com.cloudogu.sos.pipebuildlib.
-import com.cloudogu.sos.pipebuildlib.dogu.**
+import com.cloudogu.sos.pipebuildlib.dogu.*
 import com.cloudogu.ces.cesbuildlib.*
 import com.cloudogu.ces.dogubuildlib.*
 
