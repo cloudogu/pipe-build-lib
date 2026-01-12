e57b8f17ab0f2a320a22aae8f87cea8500dd2148
Dennis Schwarzer <dennis.schwarzer@cloudogu.com>
2026-01-12 14:00:21 +0100

    big refactor

--- src/com/cloudogu/sos/pipebuildlib/stages/IntegrationStages.groovy
+++ src/com/cloudogu/sos/pipebuildlib/stages/IntegrationStages.groovy
@@ -3,6 +3,9 @@ package com.cloudogu.sos.pipebuildlib.stages
 import com.cloudogu.sos.pipebuildlib.StageGroup
 import com.cloudogu.sos.pipebuildlib.DoguPipe
 
+import com.cloudogu.ces.cesbuildlib.*
+import com.cloudogu.ces.dogubuildlib.*
+
 class IntegrationStages implements DoguStageModule {
 
     void register(DoguPipe pipe, StageGroup group) {
