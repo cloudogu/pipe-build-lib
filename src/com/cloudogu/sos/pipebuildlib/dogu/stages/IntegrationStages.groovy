b95d9a74a3eb1a2202d7a290e8dcb59d5443a818
Dennis Schwarzer <dennis.schwarzer@cloudogu.com>
2026-01-12 15:16:36 +0100

    big refactor

--- src/com/cloudogu/sos/pipebuildlib/dogu/stages/IntegrationStages.groovy
+++ src/com/cloudogu/sos/pipebuildlib/dogu/stages/IntegrationStages.groovy
@@ -229,7 +229,7 @@ class IntegrationStages implements DoguStageModule {
             }
         } else if (pipe.gitflow.isPreReleaseBranch()) {
             group.stage("Push Prerelease Dogu to registry") {
-                pipe.ecoSystem.pushPreRelease(doguDir)
+                pipe.ecoSystem.pushPreRelease(pipe.doguDir)
             }
         }
     }
