02c6b6ea0cee5d4806579b00b31bb0306f46cf32
Dennis Schwarzer <dennis.schwarzer@cloudogu.com>
2026-01-12 13:58:29 +0100

    big refactor

--- src/com/cloudogu/sos/pipebuildlib/stages/DoguStageModule.groovy
+++ src/com/cloudogu/sos/pipebuildlib/stages/DoguStageModule.groovy
@@ -1,5 +1,8 @@
 package com.cloudogu.sos.pipebuildlib.stages
 
+import com.cloudogu.sos.pipebuildlib.StageGroup
+import com.cloudogu.sos.pipebuildlib.DoguPipe
+
 interface DoguStageModule {
     void register(DoguPipe pipe, StageGroup group)
 }
